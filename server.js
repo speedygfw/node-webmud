87e60ab384663e8381e3e2c3e9d55eb1f8860b89
fwallenborn <f.wallenborn@standleitungen.de>
2012-01-27 14:28:31 +0100

    bla

--- server.js
+++ server.js
@@ -1,19 +1,10 @@
 
 var http = require('http');
-var redis = require("redis"),
-    client = redis.createClient();
-var testa = "not redis";
 
-client.on("error", function (err) {
-    console.log("Error " + err);
-});
-
-client.set("test", "Redis", redis.print);
-client.get("test", testa);
 
 var server = http.createServer(function (req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" })
-  res.end("Hello world from "+testa+"\n");
+  res.end("Hello world from test\n");
 });
 
 server.listen(process.env.PORT || 8001);
